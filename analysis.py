07c3756a4f38fff52cde0285821148bd66af80c6
Goy Shuh Xian <50050582+Shuhxian@users.noreply.github.com>
2021-05-13 18:50:18 +0800

    Improvised multivariate

--- analysis.py
+++ analysis.py
@@ -3,13 +3,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 def multivariate(df):
-    for i in range(len(df)):
-        for hobby in df.loc[i,'What are your hobbies? (You may select more than 1)']:
-            df.loc[i,hobby]=1
-    df=df.fillna(0)
-    #plot heat map
-    f, ax = plt.subplots(figsize=(20, 20))
-    g=sns.heatmap(df.corr().round(2),annot=True,cmap="RdYlGn")
+    #Plot heat map
+    f, ax = plt.subplots(figsize=(30, 30))
+    g=sns.heatmap(df.corr(),fmt='.2f',annot=True,cmap="RdYlGn")
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom + 0.5, top - 0.5)
     return df
