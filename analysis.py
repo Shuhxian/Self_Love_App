5e991d875d6aa27f5c6a1f6e78f503577bd2066c
Goy Shuh Xian <50050582+Shuhxian@users.noreply.github.com>
2021-05-13 15:41:36 +0800

    Fixed cropped bottom and edge issue

--- analysis.py
+++ analysis.py
@@ -7,6 +7,9 @@ def multivariate(df):
         for hobby in df.loc[i,'What are your hobbies? (You may select more than 1)']:
             df.loc[i,hobby]=1
     df=df.fillna(0)
-    plt.figure(figsize=(20,20))
     #plot heat map
-    g=sns.heatmap(df.corr(),annot=True,cmap="RdYlGn")
+    f, ax = plt.subplots(figsize=(20, 20))
+    g=sns.heatmap(df.corr().round(2),annot=True,cmap="RdYlGn")
+    bottom, top = ax.get_ylim()
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+    return df
