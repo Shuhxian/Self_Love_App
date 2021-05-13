c48b7d8b387fc75692341ad3b4310ce948611fde
Issac Chew <issac_chew870@hotmail.com>
2021-05-13 18:06:43 +0800

    Update Data_Normalization.py

--- Data_Normalization.py
+++ Data_Normalization.py
@@ -1,15 +1,25 @@
-#away for a while
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder  
 from sklearn.preprocessing import MinMaxScaler
 
-def data_encoding(df):
+'''def data_encoding(df):
   df1 = df.iloc[:, 1:4]
   df2 = df.iloc[:, 18:]
   df_categories = pd.concat([df1, df2], axis =1)
   df_ranges = df.iloc[:, 5:17]
-  
+  #Dummy Variable Encoding
   df_categories_encoder = pd.get_dummies(df_categories)
+  df = pd.concat([df_categories_encoder, df_ranges], axis =1)
+  return df'''
+ 
+def data_encoding(df):
+  del df['Timestamp']
+  df1 = df.iloc[:, 0:3]
+  df2 = df.iloc[:, 17:]
+  df_categories = pd.concat([df1, df2], axis =1)
+  df_ranges = df.iloc[:, 4:16]
+  #Label encoder
+  df_categories_encoder = df_categories.apply(LabelEncoder().fit_transform)
   df = pd.concat([df_categories_encoder, df_ranges], axis =1)
   return df
 
